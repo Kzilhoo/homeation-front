d46a80dbe99da0b03d61ee34aba022de1ef25f12
Adrien Excoffier <adrien.excoffier@gmail.com>
2018-10-11 18:01:40 +0200

    adding vuetifiy

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import App from './App'
 import router from './router'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vuetify from 'vuetify'
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
