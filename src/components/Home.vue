c0f4bf93d381a7b71446efaa000ba763c49fbf99
Adrien Excoffier <adrien.excoffier@gmail.com>
2018-10-11 17:44:43 +0200

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Bienvenue sur votre gestionnaire domotique mdr</p>
+        <div v-for="product in products" :key=product.id>
+            <product-component :product="product" />
+        </div>
+    </div>
+</template>
+<script>
+import ProductComponent from './Product.vue'
+export default {
+  data () {
+    return {
+      products: [{id: 1, nom: 'Lampe', status: 'Allumé', image: '/static/lampe.jpeg'}, {id: 2, nom: 'Cafetière', status: 'Eteint', image: '/static/pingu.jpeg'}]
+    }
+  },
+  components: {ProductComponent}
+}
+</script>
+<style scoped>
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="product">
+        <h1>{{ product.nom }}</h1>
+        <p><img :src="product.image"></p>
+        <p>{{ product.status }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  props: ['product']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
